91843f80329b54eee14b35b5520eb86409006324
prouty <nate.prouty@infonate.com>
2013-12-14 22:23:07 -0500

    Implemented basic Sign out functionality.

--- app/screens/signout.rb
+++ app/screens/signout.rb
@@ -1,34 +1,10 @@
 module Screen
-  class Signout < PM::GroupedTableScreen
-    title 'Sign Out'
-    def will_appear
-      mm_drawerController.title = title
-    end
+  class Signout < UIViewController
+    include ProMotion::ScreenModule
 
-    stylesheet :schedule_styles
-    include Teacup::TableViewDelegate
-
-    def will_appear
-      # move down for nav menu
-      table_view.top = TOP_BELOW_MM_NAV
-      table_view.height = table_view.superview.height - table_view.top
-    end
-
-    def table_data
-      [{
-        cells: [
-          { title: "...", action: :tapped_first_name },
-        ]
-      }]
-    end
-
-    def tableView(table_view, viewForHeaderInSection:section)
-      UILabel.alloc.init.tap do |view|
-        view.backgroundColor = UIColor.clearColor
-        view.textColor = BW.rgb_color(239, 69, 67)
-        view.textAlignment = NSTextAlignmentCenter
-        view.text = section_at_index(section)[:title]
-      end
+    def viewDidLoad
+      App::Persistence.delete(:authentication_token)
+      open_screen RootScreen.new(nav_bar: true)
     end
   end
 end
