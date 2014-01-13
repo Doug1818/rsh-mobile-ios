ca1eeb6ddcfb3e2c7247fd68fa3c7ab0828dedae
prouty <nate.prouty@infonate.com>
2014-01-12 23:41:30 -0500

    Allow user to update their phone and timezone.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User
     App::Persistence[:user_id] = self.id if self.id
     App::Persistence[:user_first_name] = self.first_name if self.first_name
     App::Persistence[:user_last_name] = self.last_name if self.last_name
+    App::Persistence[:user_full_name] = "#{ self.first_name } #{ self.last_name }" if self.first_name and self.last_name
     App::Persistence[:user_phone] = self.phone if self.phone
     App::Persistence[:user_timezone] = self.timezone if self.timezone
   end
@@ -43,16 +44,18 @@ class User
     end
   end
 
+  def self.update_profile(data, &block)
 
-  # def update(data, &block)
-  #   data.merge( { authentication_token: App::Persistence[:program_authentication_token]} )
+    data.merge!({ authentication_token: App::Persistence[:program_authentication_token] })
 
-  #   BW::HTTP.put("#{Globals::API_ENDPOINT}/users/#{self.id}", { payload: data }) do |response|
-  #     if response.ok?
-  #       block.call(true, user)
-  #     else
-  #       block.call(false, nil)
-  #     end
-  #   end
-  # end
+    BW::HTTP.put("#{Globals::API_ENDPOINT}/users/#{App::Persistence[:user_id]}", { payload: data }) do |response|
+      if response.ok?
+        json_data = BW::JSON.parse(response.body.to_str)[:data]
+        user = json_data[:user]
+        block.call(true, user)
+      else
+        block.call(false, nil)
+      end
+    end
+  end
 end
--- app/screens/profile_screen.rb
+++ app/screens/profile_screen.rb
@@ -2,7 +2,7 @@ module Screen
  class ProfileScreen < PM::Screen
 
     stylesheet :profile_styles
-    TAGS = { profile_name: 2, profile_email: 3, profile_image: 4, profile_phone: 5, profile_timezone: 6 }
+    TAGS = { container_view: 1, profile_name: 2, profile_email: 3, profile_image: 4, profile_phone_field: 5, profile_timezone_field: 6 }
 
     def on_load
       self.title = ''
@@ -18,21 +18,34 @@ module Screen
 
       add_to self.view, @views[0]
 
-      profile_email = view.viewWithTag TAGS[:profile_email]
-      profile_name = view.viewWithTag TAGS[:profile_name]
-      profile_phone = view.viewWithTag TAGS[:profile_phone]
+      @container = view.viewWithTag TAGS[:container_view]
+
+      @profile_email          = view.viewWithTag TAGS[:profile_email]
+      @profile_name           = view.viewWithTag TAGS[:profile_name]
+      @profile_phone_field    = view.viewWithTag TAGS[:profile_phone_field]
+      @profile_timezone_field = view.viewWithTag TAGS[:profile_timezone_field]
+
       @profile_image = view.viewWithTag TAGS[:profile_image]
 
+      @profile_phone_field.delegate = self
+      @profile_timezone_field.delegate = self
 
       User.get_profile do |success, user|
         if success
-          profile_email.text = user.email
-          profile_name.text = "#{ user.first_name } #{ user.last_name }"
-          profile_phone.text = user.phone
+          user.persist_data
+
+          @profile_email.text           = App::Persistence[:user_email]
+          @profile_name.text            = App::Persistence[:user_full_name]
+          @profile_phone_field.text     = App::Persistence[:user_phone]
+          @profile_timezone_field.text  = App::Persistence[:user_timezone]
+
           unless user.avatar.nil?
             image_url = user.avatar
-            image_data = NSData.alloc.initWithContentsOfURL(NSURL.URLWithString(image_url))
-            @profile_image.setBackgroundImage(UIImage.imageWithData(image_data), forState: UIControlStateNormal)
+
+            unless image_url == '/assets/default-avatar.png'
+              image_data = NSData.alloc.initWithContentsOfURL(NSURL.URLWithString(image_url))
+              @profile_image.setBackgroundImage(UIImage.imageWithData(image_data), forState: UIControlStateNormal)
+            end
           end
         else
           App.alert("oops")
@@ -45,26 +58,80 @@ module Screen
     def chooseProfileImage(sender)
       BW::Device.camera.any.picture(media_types: [:image]) do |result|
         image_view = UIImageView.alloc.initWithImage(result[:original_image])
-        image = UIImageJPEGRepresentation(image_view.image, 9);
+        image = UIImageJPEGRepresentation(image_view.image, 1);
 
         if image   
           encodedData = [image].pack("m0")
 
-          data = {
-            authentication_token: App::Persistence[:program_authentication_token],
-            avatar: encodedData
-          }
-          BW::HTTP.put("#{Globals::API_ENDPOINT}/users/#{App::Persistence[:user_id]}", {payload: data}) do |response|
-            if response.ok?
-              json_data = BW::JSON.parse(response.body.to_str)[:data]
-              user = json_data[:user]
+          data = { user: { image_data: encodedData }}
+
+          User.update_profile(data) do |success, user|
+            if success
               image_url = user[:avatar][:large][:url]
               image_data = NSData.alloc.initWithContentsOfURL(NSURL.URLWithString(image_url))
               @profile_image.setBackgroundImage(UIImage.imageWithData(image_data), forState: UIControlStateNormal)
+            else
+              NSLog("Error updating profile.")
             end
           end
         end
       end
+    end
+
+    # form submission
+    def textFieldShouldReturn(textField)
+
+      data = {}
+
+      data[:user] = case textField
+      when @profile_phone_field
+        { phone: textField.text }
+      when @profile_timezone_field
+        { timezone: textField.text }
+      else
+      end
+
+      User.update_profile(data) do |success, user|
+        if success
+          NSLog("User profile updated.")
+        else
+          NSLog("Error updating user profile.")
+        end
+      end
+
+      container_frame = @container.frame
+      container_frame.origin.y += 200
+
+      UIView.animateWithDuration(0.3,
+        animations: lambda {
+          @container.frame = container_frame
+        },
+        completion: lambda { |finished|
+          @offset = false
+        }
+      )
+
+      # this will hide the keyboard
+      textField.resignFirstResponder
+    end
+
+    def textFieldDidBeginEditing(textField)
+      # if we've already shifted the view up, don't do it again
+      return if @offset
+
+      # grab our current frame and modify it so it's visible
+      container_frame = @container.frame
+      container_frame.origin.y -= 200
+
+      # animate the replacement of the current frame with the new one
+      UIView.animateWithDuration(0.3,
+        animations: lambda {
+          @container.frame = container_frame
+        },
+        completion: lambda { |finished|
+          @offset = true
+        }
+      )
     end
   end
 end
