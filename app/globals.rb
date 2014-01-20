616f33aefb923097e92b094b1af047804274fec3
Doug Raicek <doug.raicek@gmail.com>
2014-01-20 14:02:11 -0500

    Change api endpoint to mystepsapp.com

--- app/globals.rb
+++ app/globals.rb
@@ -2,5 +2,6 @@ class Globals
   # API_ENDPOINT = "http://localhost:3000/api/v1"
   # API_ENDPOINT = "https://rsh-nate.ngrok.com/api/v1"
   # API_ENDPOINT = "https://rsh-adam.ngrok.com/api/v1"
-  API_ENDPOINT = "http://rightsidehealth.herokuapp.com/api/v1"
+  # API_ENDPOINT = "https://rsh-doug.ngrok.com/api/v1"
+  API_ENDPOINT = "http://mystepsapp.com/api/v1"
 end
