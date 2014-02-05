4ad8bbea894a8f494b35930af336890a39828857
Doug Raicek <doug.raicek@gmail.com>
2014-02-05 15:18:18 -0500

    Add new attachment modal

---
+++ app/screens/attachment_screen.rb
@@ -0,0 +1,27 @@
+class AttachmentScreen < PM::WebScreen
+	attr_accessor :url
+
+	title "Attachment"
+
+	def on_load
+		set_nav_bar_button :right, title: "Done", action: :close_attachment_screen
+	end
+
+	def content
+		# UIApplication.sharedApplication.openURL(NSURL.URLWithString(url))
+		NSURL.URLWithString(url)
+	end
+
+	def load_failed
+		UIAlert.alloc.initWithTitle('Failed to load',
+			message: 'Sorry, the attachment failed to load. Please check that you have an internet connection.',
+			delegate: nil,
+			cancelButtonTitle: 'OK',
+			otherButtonTitles: nil
+		).show
+	end
+
+	def close_attachment_screen
+		close
+	end
+end
